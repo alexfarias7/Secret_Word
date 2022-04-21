3d4d0be7bcf96231b515f08293893342694010bb
alexfarias7 <alexfarias44@gmail.com>
2022-04-21 04:11:22 -0300

    structuring the game component

--- src/components/Game/index.jsx
+++ src/components/Game/index.jsx
@@ -1,10 +1,33 @@
-import styles from './style.module.css'
+import styles from "./style.module.css";
 
-const Game = ({verifyletter}) => {
-    return ( <div>
-        <h1>game</h1>
-        <button onClick={verifyletter}>terminar</button>
-    </div> );
-}
- 
+const Game = ({ verifyletter }) => {
+  return (
+    <div className={styles.game}>
+      <p className={styles.points}>
+        <span>Pontuação: 000</span>
+      </p>
+      <h1> Advinhe a palavra</h1>
+      <h3 className={styles.tip}></h3>
+      <div className={styles.worldContainer}>
+        <span className={styles.letter}>B</span>
+        <span className={styles.blankSquare}></span>
+      </div>
+      <div className={styles.letterContainer}>
+        <p>Tente advinhar uma palavra:</p>
+        <form>
+          <input type="text" name="letter" maxLength={1} required />
+          <button>Jogar</button>
+        </form>
+
+        <div className={styles.wrongLettersContainer}>
+            <p>letras já utilizadas:</p>
+            <span>a,</span>
+            <span>b</span>
+        </div>
+      </div>
+
+    </div>
+  );
+};
+
 export default Game;
